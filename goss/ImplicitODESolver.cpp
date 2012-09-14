afb01af655995707bcb6fc91dd7a757394217dff
Johan Hake <hake.dev@gmail.com>
2012-09-14 13:45:09 +0200

    Work on ImplicitODESolver

--- goss/ImplicitODESolver.cpp
+++ goss/ImplicitODESolver.cpp
@@ -1,169 +1,203 @@
-// This class needs some serious reconsidderation regarding memory use!!
-
+// FIXME: This class needs some serious reconsideration regarding memory use!!
 
 #include "ImplicitODESolver.h"
 //#include <iostream.h>
 #include <stdio.h>
-
+#include <cmath>
 
 using namespace goss;
 
-ImplicitODESolver:: ImplicitODESolver()
-  : ODESolver(0.0, 0.0), jac(0), f1(0), f2(0), yz(0), b(0), dz(0), prev(0),
-    newton_tol(0.0), eta(1.0), kappa(0.1), stages(0), newtonits(0), maxits(0), rejects(0),
-    jac_comp(0), min_dt(0.0), recompute_jacobian(true)
+//-----------------------------------------------------------------------------
+ImplicitODESolver::ImplicitODESolver()
+  : ODESolver(0.0, 0.0), jac(0), f1(0), f2(0), yz(0), _b(0), dz(0), _prev(0),
+    _newton_tol(1.e-5), eta(1e-10), kappa(0.1), stages(0), newtonits(0), 
+    maxits(10), rejects(0), jac_comp(0), min_dt(0.0), recompute_jacobian(true)
 {
   // Do nothing
 }
-
-
-ImplicitODESolver:: ImplicitODESolver(double ldt)
-  : ODESolver(ldt), jac(0), f1(0), f2(0), yz(0), b(0), dz(0), prev(0),
-    newton_tol(0.0), eta(1.0), kappa(0.1), stages(0), newtonits(0), maxits(0), rejects(0),
-    jac_comp(0), min_dt(0.0), recompute_jacobian(true)
+//-----------------------------------------------------------------------------
+ImplicitODESolver::ImplicitODESolver(double ldt)
+  : ODESolver(ldt), jac(0), f1(0), f2(0), yz(0), _b(0), dz(0), _prev(0),
+    _newton_tol(1.e-5), eta(1e-10), kappa(0.1), stages(0), newtonits(0), 
+    maxits(10), rejects(0), jac_comp(0), min_dt(0.0), recompute_jacobian(true)
 {
   // Do nothing
 }
-
-void ImplicitODESolver:: init(){
-  maxits = 10;
+//-----------------------------------------------------------------------------
+ImplicitODESolver::~ImplicitODESolver ()
+{
+  // Destroy jacobian
+  if (jac) 
+  {
+    for (uint i=0; i < jac_size; ++i)
+      delete[] jac[i];
+  }
+  
+  if (jac)  delete[] jac;
+  if (f1)   delete[] f1;
+  if (f2)   delete[] f2;
+  if (yz)   delete[] yz;
+  if (_b)    delete[] _b; 
+  if (dz)   delete[] dz;
+  if (_prev) delete[] _prev;
+}
+//-----------------------------------------------------------------------------
+void ImplicitODESolver::init()
+{
   rejects = 0;
   jac_comp = 0;
-  eta      = 1e-10;
-  kappa = 1e-1;
-  recompute_jacobian = true;//We need to compute the Jacobian the first time
 
+  // We need to compute the Jacobian the first time
+  recompute_jacobian = true;
 
+  _b   = new double[_ode->size()]; 
+  dz   = new double[_ode->size()];
+  _prev = new double[_ode->size()];
 
-  b    = static_cast<double*>(malloc(sizeof(double)*ode->size()));
-  dz   = static_cast<double*>(malloc(sizeof(double)*ode->size()));
-  prev = static_cast<double*>(malloc(sizeof(double)*ode->size()));  
+  yz   = new double[_ode->size()];
+  f1   = new double[_ode->size()];
+  f2   = new double[_ode->size()];
 
-  yz   = static_cast<double*>(malloc(sizeof(double)*ode->size()));
-  f1   = static_cast<double*>(malloc(sizeof(double)*ode->size()));
-  f2   = static_cast<double*>(malloc(sizeof(double)*ode->size()));
+  // Init jacobian
+  jac_size = _ode->size();
+  jac  = new double*[_ode->size()];
+  for (uint i = 0; i < _ode->size(); ++i)
+    jac[i] = new double[_ode->size()];
 
-  jac  = static_cast<double**>(malloc(sizeof(double*)*ode->size()));
-  jac_size = ode->size();
-  for (int i = 0; i < ode->size(); ++i)
-    jac[i] = static_cast<double*>(malloc(sizeof(double)*ode->size()));
-
-  setNewtonTol();
-
+  // Newton tolerance
+  _newton_tol = 1.e-5;
+  
 }
-
-void ImplicitODESolver:: computeJacobian(double t, double* y){
-
-  int i,j;
-  double max,ysafe,delta;
-  ode->eval(y,t, f1);
-  for (i=0;i<ode->size();++i){
+//-----------------------------------------------------------------------------
+void ImplicitODESolver::compute_jacobian(double t, double* y)
+{
+  uint i, j;
+  double max, ysafe, delta;
+  _ode->eval(y, t, f1);
+  
+  for (i = 0; i < _ode->size(); ++i)
+  {
     ysafe = y[i];
-    max=((1e-5>fabs(ysafe))?1e-5:fabs(ysafe));
-    delta=sqrt(1e-15*max);
+    max = 1e-5 > std::fabs(ysafe) ? 1e-5 : std::fabs(ysafe);
+    delta = std::sqrt(1e-15*max);
     y[i] += delta;
-    ode->eval(y, t, f2);
-    for (j=0;j<ode->size();++j){
+    _ode->eval(y, t, f2);
+    
+    for (j=0;j<_ode->size();++j)
       jac[j][i]=(f2[j]-f1[j])/delta;
-    }
+    
     y[i]=ysafe;
   } 
 }
-
-void ImplicitODESolver:: mult(double fact, double** matrix){
-  int i,j;
-  for (i = 0; i < ode->size(); ++i)
-    for (j = 0; j < ode->size(); ++j)
+//-----------------------------------------------------------------------------
+void ImplicitODESolver::mult(double fact, double** matrix)
+{
+  for (uint i = 0; i < _ode->size(); ++i)
+    for (uint j = 0; j < _ode->size(); ++j)
       matrix[i][j] *= fact;
 }
-
-void ImplicitODESolver:: addIdentity(double** matrix){
-  int i;
-  for (i = 0; i < ode->size(); ++i)
+//-----------------------------------------------------------------------------
+void ImplicitODESolver::add_identity(double** matrix)
+{
+  for (uint i = 0; i < _ode->size(); ++i)
     matrix[i][i] += 1;
 }
-
-
-void ImplicitODESolver:: factLU(double** mat){
-  int i, k, r;
+//-----------------------------------------------------------------------------
+void ImplicitODESolver::lu_factorize(double** mat)
+{
   double sum;
 
+  for (uint k = 1; k < _ode->size(); k++)
+  {
 
-  for (k = 1; k < ode->size(); k++){
-    for (i = 0; i <= k-1; ++i){
+    for (uint i = 0; i <= k-1; ++i)
+    {
       sum = 0.0;
-      for (r = 0; r <= i-1; r++)
+      for (uint r = 0; r <= i-1; r++)
         sum += mat[i][r]*mat[r][k];
+    
       mat[i][k] -=sum;
       sum = 0.0;
-      for (r = 0; r <= i-1; r++)
+
+      for (uint r = 0; r <= i-1; r++)
         sum += mat[k][r]*mat[r][i];
+    
       mat[k][i] = (mat[k][i]-sum)/mat[i][i];
+
     }
-    sum = 0;
-    for (r = 0; r <= k-1; r++)
+
+    sum = 0.0;
+    for (uint r = 0; r <= k-1; r++)
       sum += mat[k][r]*mat[r][k];
+
     mat[k][k] -= sum;
+
   }
-
-
 }
-
-void ImplicitODESolver::forwBackLU(const double * const * mat, double* b, double* x){
-  //solves Ax = b with forward backward substitution, provided that 
-  //A is already L1U factorized
+//-----------------------------------------------------------------------------
+void ImplicitODESolver::forward_backward_subst(const double * const * mat, 
+					       double* b, double* x)
+{
+  // solves Ax = b with forward backward substitution, provided that 
+  // A is already L1U factorized
 
   double sum;
-
-  int i,j;
 
   x[0] = b[0];
 
-  for (i = 1; i < ode->size(); ++i){
-    sum = 0;
-    for (j = 0; j <= i-1; ++j)
+  for (uint i = 1; i < _ode->size(); ++i)
+  {
+    sum = 0.0;
+    for (uint j = 0; j <= i-1; ++j)
       sum = sum + mat[i][j]*x[j];
+
     x[i] = b[i] -sum;
   }
-  x[ode->size()-1] = x[ode->size()-1]/mat[ode->size()-1][ode->size()-1];
 
+  x[_ode->size()-1] = x[_ode->size()-1]/mat[_ode->size()-1][_ode->size()-1];
 
-  for (i = ode->size()-2; i >=0; i--){
+  for (int i = _ode->size()-2; i >= 0; i--)
+  {
     sum = 0;
-    for (j = i+1; j < ode->size(); ++j)
+    for (uint j = i + 1; j < _ode->size(); ++j)
       sum = sum +mat[i][j]*x[j];
+  
     x[i] = (x[i]-sum)/mat[i][i];
   }
-
 }
-
-
-bool ImplicitODESolver::NewtonSolve(double* z, double* prev, double* y0, double t, double dt, double alpha){
-  int i;
+//-----------------------------------------------------------------------------
+bool ImplicitODESolver::newton_solve(double* z, double* prev, double* y0, double t,
+				     double dt, double alpha)
+{
+  uint i;
   bool step_ok = true, converged = false;
   newtonits = 0;
-  double Ntheta, z_norm, prev_norm;
-  recompute_jacobian=false;
+  double Ntheta, z_norm, prev_norm = 1.0;
+  recompute_jacobian = false;
 
-  do{
-    for (i = 0; i < ode->size(); ++i)
-      yz[i]=y0[i]+z[i];
-    ode->eval(yz,t,f1);
-    for (i = 0; i < ode->size(); ++i)
-      b[i] = -z[i]+dt*(prev[i]+alpha*f1[i]);
+  do
+  {
+    for (i = 0; i < _ode->size(); ++i)
+      yz[i] = y0[i] + z[i];
 
-    forwBackLU(jac,b,dz);
+    _ode->eval(yz,t,f1);
+    for (i = 0; i < _ode->size(); ++i)
+      _b[i] = -z[i] + dt*(prev[i] + alpha*f1[i]);
+
+    forward_backward_subst(jac, _b, dz);
     z_norm = norm(dz);
 
-
-
-    if (newtonits > 0) {
+    if (newtonits > 0) 
+    {
       Ntheta = z_norm/prev_norm;
+
       if (Ntheta<1e-3)
-        recompute_jacobian=false;
+        recompute_jacobian = false;
       else
-        recompute_jacobian=true;
-      if (Ntheta >1){
+        recompute_jacobian = true;
+    
+      if (Ntheta >1)
+      {
 #ifdef DEBUG
         printf("Newton solver diverges with Ntheta = %1.2e, reduces time step\n", Ntheta);
 #endif
@@ -172,22 +206,32 @@ bool ImplicitODESolver::NewtonSolve(double* z, double* prev, double* y0, double 
         //return step_ok;
         break;
       }
-      if (z_norm>(kappa*newton_tol*(1-Ntheta)/pow(Ntheta,maxits-newtonits))){
+      
+      if (z_norm > (kappa*_newton_tol*(1 - Ntheta)/std::pow(Ntheta, maxits - newtonits)))
+      {
 #ifdef DEBUG
-        printf("Newton solver converges to slow with theta = %1.2e at iteration %d, reduces time step\n", Ntheta,newtonits);
+        printf("Newton solver converges to slow with theta = %1.2e at "
+	       "iteration %d, reduces time step\n", Ntheta, newtonits);
 #endif
         rejects ++;
         step_ok = false;
         recompute_jacobian=true;
         break;
       }
+      
       eta = Ntheta/(1.0-Ntheta);
-    }else{
-      eta = (eta > 1e-15 ? eta : 1e-15);
-      eta = pow(eta,0.8);
+    }
+    
+    // newtonits == 0
+    else
+    {
+      eta = eta > 1e-15 ? eta : 1e-15;
+      eta = std::pow(eta, 0.8);
     }
 
-    if (newtonits > maxits && !converged){
+    // No convergence
+    if (newtonits > maxits && !converged)
+    {
 #ifdef DEBUG
       printf("Not converged in %d iterations. Reduces time step.\n",maxits);
 #endif
@@ -196,28 +240,26 @@ bool ImplicitODESolver::NewtonSolve(double* z, double* prev, double* y0, double 
       //return step_ok;
       break;
     }
-    for (i = 0; i <ode->size(); ++i)
+    
+    for (i = 0; i <_ode->size(); ++i)
       z[i] += dz[i];
 
     prev_norm = z_norm;
     newtonits++;
-  } while (eta*z_norm <= kappa*newton_tol); 
 
+  } while (eta*z_norm <= kappa*_newton_tol); 
 
   return step_ok;
 }
-
-
-
-
-
-double ImplicitODESolver:: norm(double* vec)
+//-----------------------------------------------------------------------------
+double ImplicitODESolver::norm(double* vec)
 {
-  double l2_norm;
+  double l2_norm = 0;
 
-  for (int i = 0; i < ode->size(); ++i)
+  for (uint i = 0; i < _ode->size(); ++i)
     l2_norm += vec[i]*vec[i];
 
-  l2_norm = sqrt(l2_norm);
+  l2_norm = std::sqrt(l2_norm);
   return l2_norm;
 }
+//-----------------------------------------------------------------------------
--- goss/ImplicitODESolver.h
+++ goss/ImplicitODESolver.h
@@ -2,66 +2,79 @@
 #define ImplicitODESolver_h_IS_INCLUDED
 
 #include "ODESolver.h"
-#include <iostream>
-#include <math.h>
-#include <cstdlib>
 
 namespace goss 
 {
 
-  class ImplicitODESolver :public ODESolver {
-    public:
-      void setNewtonTol(double newton_tol_=1e-5){newton_tol=newton_tol_;}
+  // Base class of all Implicit Solvers
+  class ImplicitODESolver : public ODESolver 
+  {
+  
+  public:
+  
+    // Constructor
+    ImplicitODESolver();
+  
+    // Constructor
+    ImplicitODESolver(double ldt);
 
-      ~ImplicitODESolver () {
-        if (jac != NULL) {
-          for (int i=0;i< jac_size; ++i)
-            free(jac[i]);
-        }
-        free(jac);
-        free(f1);
-        free(f2);
-        free(yz);
-        free(b); 
-        free(dz);
-        free(prev);
-      }
+    // Destructor
+    virtual ~ImplicitODESolver ();
 
-      ImplicitODESolver();
-      ImplicitODESolver(double ldt);
+    // Set newton tolerance
+    void set_newton_tol(double newton_tol){_newton_tol = newton_tol;}
 
-    protected: 
+  protected: 
 
-      //variables used in the jacobian evaluation
-      double **jac;
-      double *f1, *f2, *yz;
-      double dt_old; //h; 
+    // Variables used in the jacobian evaluation
+    double **jac;
+    double *f1, *f2, *yz;
+    double dt_old;
 
-      double *b, *dz; // right hand side and solution of the linear system
-      double *prev; //previous stages, used by DIRK methods
-      double newton_tol;
-      double eta;// Variable used in the estimation of the error of the newton iteration for the first iteration (Very important for linear problems!)
-      double kappa;// The safety factor for the stopping criterion of the newton iteration
-      int jac_size;
+    // Right hand side and solution of the linear system
+    double *_b, *dz;
 
-      int stages;
-      int newtonits, maxits, rejects, jac_comp;
-      double  min_dt;
-      bool recompute_jacobian;
+    // Previous stages, used by DIRK methods
+    double *_prev;
 
-      void computeJacobian(double t, double* y);
-      void mult(double fact, double** matrix);
-      void addIdentity(double** matrix);
+    // Newton tolerance
+    double _newton_tol;
 
-      void factLU(double** mat);
-      void forwBackLU(const double * const * mat, double* b, double* x);
+    // Variable used in the estimation of the error of the newton 
+    // iteration for the first iteration (Important for linear problems!)
+    double eta;
 
-      void init(); 
+    // The safety factor for the stopping criterion of the newton iteration
+    double kappa;
+    uint jac_size;
 
+    uint stages;
+    int newtonits, maxits, rejects, jac_comp;
+    double  min_dt;
+    bool recompute_jacobian;
 
-      //this function is designed for SDIRK and Backward Euler:
-      virtual bool NewtonSolve(double* k, double* prev, double* y0, double t, double dt, double alpha);
-      virtual double norm(double* vec);
+    // Compute jacobian using numerical approximation
+    void compute_jacobian(double t, double* y);
+
+    // Scale a matrix
+    void mult(double fact, double** matrix);
+
+    // Add identity to matrix
+    void add_identity(double** matrix);
+
+    // LU Factorize matrix
+    void lu_factorize(double** mat);
+
+    // Forward/Backward supstituion of factories matrix
+    void forward_backward_subst(const double* const* mat, double* b, double* x);
+
+    // Init solver
+    void init(); 
+
+    // This function is designed for SDIRK and Backward Euler:
+    virtual bool newton_solve(double* k, double* prev, double* y0, double t, 
+			      double dt, double alpha);
+    virtual double norm(double* vec);
   };
 
 }
