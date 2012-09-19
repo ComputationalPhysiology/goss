a6e071cb5b1b8e120dfcb3226ee15388d6e59015
Johan Hake <hake.dev@gmail.com>
2012-09-19 20:58:14 +0200

    Work on ParameterizedODE

--- goss/ParameterizedODE.h
+++ goss/ParameterizedODE.h
@@ -14,8 +14,17 @@ namespace goss {
   class ParameterizedODE : public virtual ODE
   {
   public:
-    ParameterizedODE(uint system_size, uint parameter_size) : 
-      ODE(system_size), _parameter_size(parameter_size)
+
+    // Constructor
+    ParameterizedODE(uint system_size, uint num_parameters_, uint num_field_states_, 
+		     uint num_field_parameters_, uint num_intermediates_) : 
+      ODE(system_size), 
+      _state_names(system_size, ""), 
+      _field_state_names(num_field_states_, ""), 
+      _parameter_names(num_parameters_, ""), 
+      _field_parameter_names(num_field_parameters_, ""), 
+      _field_state_indices(num_field_states_, 0), 
+      _intermediate_names(num_intermediates_, "")
     { 
       // Do nothing
     } 
@@ -25,8 +34,17 @@ namespace goss {
       // Do nothing
     }
 
+    // Return the number of field states
+    inline uint num_field_states() const { return _field_state_names.size(); }
+
     // The number of parameters
-    inline uint num_parameters() const {return _parameter_size;}
+    inline uint num_parameters() const { return _parameter_names.size(); }
+
+    // The number of field parameters
+    inline uint num_field_parameters() const { return _field_parameter_names.size(); }
+
+    // The number of field parameters
+    inline uint num_intermediates() const { return _intermediate_names.size(); }
 
     // Evaluate the intermediates
     virtual void eval_intermediates(const double* x, double t, double* y) const = 0;
@@ -34,41 +52,61 @@ namespace goss {
     // Evaluate componentwise intermediates
     virtual double eval_intermediate(uint i, const double* x, double t) const = 0;
 
-    // Set all parameters
-    virtual void set_parameters(const double* values) = 0;
+    // Set all field parameters
+    virtual void set_field_parameters(const double* values) = 0;
 
-    // Set single parameter
-    virtual void set_parameter(uint idx, double value) = 0;
+    // Set a parameter
+    virtual void set_parameter(std::string name, double value) = 0;
 
-    // Get parameter idx
-    virtual double get_parameter(uint idx) const = 0;
+    // Get a parameter value
+    virtual double get_parameter(std::string name) const = 0;
 
-    // Get all parameters
-    virtual void get_parameters(goss::DoubleVector*) const = 0;
+    // Get all state names
+    const std::vector<std::string>& get_state_names() const
+    { return _state_names; }
 
-    // Get state name idx
-    virtual std::string get_state_name(uint idx) const;
+    // Get field state names
+    const std::vector<std::string>& get_field_state_names() const
+    { return _field_state_names; }
 
-    // Get parameter name idx
-    virtual std::string get_parameter_name(uint idx) const;
+    // Get all parameter names
+    const std::vector<std::string>& get_parameter_names() const
+    { return _parameter_names; }
+
+    // Get field parameter names
+    const std::vector<std::string>& get_field_parameter_names() const
+    { return _field_parameter_names; }
+
+    // Get field parameter names
+    const std::vector<uint>& get_field_state_indices() const
+    { return _field_state_indices; }
+
+    // Get intermediate names
+    const std::vector<std::string>& get_intermediate_names() const
+    { return _intermediate_names; }
 
   protected: 
     
-    // FIXME: Force user to fill these vectors in constructor?
-    // Set state descriptions
-    virtual void set_state_descriptions() = 0;
-    
-    // Set parameter names
-    virtual void set_parameter_names() = 0;
-    
-    // Number of parameters
-    const uint _parameter_size;
-    
-    // State descriptions
-    std::vector<std::string> _state_descr;
-    
-    // Parameter descriptions
-    std::vector<std::string> _parameter_descr;
+    // These vectors are initialized in this class, but need to be 
+    // filled in derived classes.
+
+    // Vector with state names
+    std::vector<std::string> _state_names;
+
+    // Vector with field state names
+    std::vector<std::string> _field_state_names;
+
+    // Vector with all parameter names
+    std::vector<std::string> _parameter_names;
+
+    // Vector with field parameter names
+    std::vector<std::string> _field_parameter_names;
+
+    // Vector with field state indices
+    std::vector<uint> _field_state_indices;
+
+    // Vector with intermediate names
+    std::vector<std::string> _intermediate_names;
     
   };
 }
