f86aba7835484e715088210466bc7be88515061a
Johan Hake <hake.dev@gmail.com>
2012-09-21 12:50:38 +0200

    Add test of setting and getting parameters

--- test/ode/ode_test.cpp
+++ test/ode/ode_test.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "goss/goss.h"
 #include "NonLinOscillator.h"
 #include "Arenstorf.h"
@@ -19,11 +21,12 @@ using namespace goss;
 template <class O, class S>
 class ODESolverTest : public testing::Test {
 protected:
-  ODESolverTest() : solver(new S(new O())) {}
+  ODESolverTest() : ode(new O()), solver(new S(ode)) {}
 
   virtual ~ODESolverTest() { delete solver; }
 
   // ODE and solver
+  ODE* ode;
   ODESolver* solver;
   DoubleVector x_coarse, x_fine;
   
@@ -75,19 +78,29 @@ public :
   RLTester() : ODESolverTest<VDP, S>() {}
 };
 
+template<class O>
+class ParameterizedODETester : public ODESolverTest<O, RKF32>
+{
+public :
+  ParameterizedODETester() : ODESolverTest<O, RKF32>() {}
+};
+
 // The list of ODEs we want to test.
 typedef testing::Types<Arenstorf, Brusselator, NonLinOscillator, EulerRigidBody, \
 		       FG, Robertson, Sin, VDP, SaltzLorenz> ODEs;
 
 // Different list of Solvers
 typedef testing::Types<ExplicitEuler, RK2, RK4, RKF32> ExplicitODESolvers; 
-typedef testing::Types<ImplicitEuler, ESDIRK4O32> ImplicitODESolvers;
+//typedef testing::Types<ImplicitEuler, ESDIRK4O32> ImplicitODESolvers;
+typedef testing::Types<ImplicitEuler> ImplicitODESolvers;
 typedef testing::Types<RL, GRL1, GRL2> RLODESolvers;
+typedef testing::Types<Winslow> ParameterizedODEs;
 
 TYPED_TEST_CASE(ODETester, ODEs);
 TYPED_TEST_CASE(ExplicitTester, ExplicitODESolvers);
 TYPED_TEST_CASE(ImplicitTester, ImplicitODESolvers);
 TYPED_TEST_CASE(RLTester, RLODESolvers);
+TYPED_TEST_CASE(ParameterizedODETester, ParameterizedODEs);
 
 // Run all included 
 TYPED_TEST(ODETester, IntegrationTest) 
@@ -139,4 +152,47 @@ TYPED_TEST(RLTester, RLSolverTest)
 
   ASSERT_NEAR(this->x_fine.data[0], this->x_coarse.data[0], 1.0);
   
+}
+
+TYPED_TEST(ParameterizedODETester, ParameterizedODETest) 
+{
+
+  // Local version
+  ParameterizedODE& lode = *dynamic_cast<ParameterizedODE*>(this->ode);
+
+  // Run coarse simulation
+  this->run_ode(0.1, 10.0, this->x_coarse);
+
+  // Run fine simulation
+  this->run_ode(0.01, 10.0, this->x_fine);
+
+  ASSERT_NEAR(this->x_fine.data[0], this->x_coarse.data[0], 1.0);
+  
+  // Change parameters
+  
+  std::string param = lode.get_parameter_names()[0];
+  const double value = lode.get_parameter(param);
+  lode.set_parameter(param, value*0.9);
+
+  ASSERT_EQ(lode.get_parameter(param), value*0.9);
+
+  // Set field parameters
+  std::vector<double> values(lode.num_field_states(), 0.0);
+  for (uint i = 0; i < lode.num_field_states(); i++)
+  {
+    param = lode.get_field_parameter_names()[i];
+    values[i] = lode.get_parameter(param)*0.9;
+  }
+  
+  lode.set_field_parameters(&values[0]);
+  ASSERT_EQ(lode.get_parameter(param), values[values.size()-1]);
+  
+  // Run coarse simulation
+  this->run_ode(0.1, 10.0, this->x_coarse);
+
+  // Run fine simulation
+  this->run_ode(0.01, 10.0, this->x_fine);
+
+  ASSERT_NEAR(this->x_fine.data[0], this->x_coarse.data[0], 1.0);
+
 }
