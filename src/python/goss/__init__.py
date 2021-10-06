9851b6c68ad5b69cf4abd09e09d0b4fcbb3dc1a7
Henrik Finsberg <henriknf@simula.no>
2021-10-06 12:46:47 +0200

    Pybind is working

--- src/python/goss/__init__.py
+++ src/python/goss/__init__.py
@@ -20,84 +20,21 @@
 # First added:  2012-10-11
 # Last changed: 2012-10-11
 
-__all__ = []
 
 # Import pure python modules
 from . import codegeneration as codegeneration
 from . import compilemodule as compilemodule
 
-from .codegeneration import *
-from .compilemodule import *
-
-__all__.extend(codegeneration.__all__)
-__all__.extend(compilemodule.__all__)
-
-#--- Imports the SWIG-generated Python code (C++ interface) ---
-
-_not_includes = ["new_instancemethod", "SHARED_PTR_DISOWN", \
-                 "weakref", "weakref_proxy", "cvar"]
 
 from . import cpp
 
-# Add the module to the global namespace
-__all__.append("cpp")
-
-# Collect all classes to outrule static class methods
-_all_classes = []
-
-# Iterate over the modules attributes and add names to global namespace
-for name in sorted(cpp.__dict__.keys()):
-
-    # Skip some includes
-    if name in _not_includes:
-        continue
-
-    # Do not add an attrbute name which contains "swigregister" or
-    # starts with "_"
-    if "swigregister" in name or name[0] == "_":
-        continue
-
-    # Get attr
-    attr = getattr(cpp, name)
-
-    # Check for class
-    if isinstance(attr, type):
-        _all_classes.append(name)
-    else:
-        # If not a class, check if first part of function name is the
-        # same as a class name
-        if name.split("_")[0] in _all_classes:
-            continue
-        
-    # Add the attribute to the global namespace
-    globals()[name] = attr
-    __all__.append(name)
-
-# Add version 
-# -----------
-globals()["__version__"] = getattr(cpp, "__gossversion__")
-globals()["__swigversion__"] = getattr(cpp, "__swigversion__")
-
-# Get names of all valid goss.ODESolvers
-abstract_solvers = ["ImplicitODESolver", "AdaptiveImplicitSolver", "AdaptiveExplicitSolver"]
-goss_solvers = [name for name, attr in cpp.__dict__.items() \
-                 if isinstance(attr, type) and issubclass(attr, cpp.ODESolver) \
-                and name not in abstract_solvers]
-
-goss_solvers.remove("ODESolver")
-__all__.append("goss_solvers")
+__all__ = ["codegeneration", "compilemodule", "cpp"]
 
 # If dolfin is present import it
 try:
     from . import dolfinutils
-    from .dolfinutils import *
     __all__.extend(dolfinutils.__all__)
 except Exception as e:
     pass
     
 
-# Set debug level
-DEBUG = DBG
-
-# Clean up
-del name, attr
--- src/python/goss/goss.cpp
+++ src/python/goss/goss.cpp
@@ -1,12 +1,22 @@
-#include "../../../include/goss/goss.h"
+#include <goss/goss.h>
 
 #include <pybind11/stl.h>
 
 #include <pybind11/pybind11.h>
+
+
 namespace py = pybind11;
 
 void init_ExplicitEuler(py::module &m) {
     
     py::class_<goss::ExplicitEuler>(m, "ExplicitEuler")
     .def(py::init<>());
+}
+
+PYBIND11_MODULE(cpp, m) {
+
+    m.doc() = "This is a Python binding of C++ goss Library";
+
+   
+    init_ExplicitEuler(m);
 }
