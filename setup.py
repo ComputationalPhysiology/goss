17112ca40b63fd73a64081838bcc3e0d1477d1de
Henrik Finsberg <henriknf@simula.no>
2022-03-24 21:59:08 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (here / "README.md").read_text()
 setup(
     name="pygoss",
     python_requires=">=3.7.0",
-    version="0.2.0",
+    version="0.2.1",
     description="Python interface to goss - General ODE System Solver",
     long_description=long_description,
     long_description_content_type="text/markdown",
